0dae12d92d63a5fdde201478e2c202a757a6d03e
Mesha <madsketchz@gmail.com>
2023-02-24 09:07:00 -0800

    Update test files and naming conventions

---
+++ PierreBakery.Tests/ModelTests/ClassNameTests.cs
@@ -0,0 +1,22 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ProjectName.Models;
+
+namespace PierresBakery.Tests
+{
+  [TestClass]
+  public class ClassNameTests
+  {
+    [TestMethod]
+    public void
+NameOfMethodWeAreTesting_DescriptionOfBehavior_ExpectedReturnValue()
+    {
+      // Arrange
+
+      // Act
+
+      // Assert 
+      Assert.AreEqual(ExpectedResult, CodeToTest);
+    }
+
+  }
+}
--- PierreBakery/Program.cs
+++ PierreBakery/Program.cs
@@ -1,5 +1,5 @@
 using System;
-using Projectname.Models;
+using PierresBakery.Models;
 using System.Collections.Generic;
 
   class Program
